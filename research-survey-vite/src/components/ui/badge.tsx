1e83f6079dea17304c20301e46279f68c945d1ae
laurl1lleoja <51074342+laurl1lleoja@users.noreply.github.com>
2026-03-16 11:55:30 +0200

    Delete research-survey-vite directory

--- research-survey-vite/src/components/ui/badge.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function Badge({ className = "", ...props }: React.HTMLAttributes<HTMLSpanElement>) {
-  return <span className={className} {...props} />;
-}
--- research-survey-vite/src/components/ui/input.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(function Input({ className = "", type = "text", ...props }, ref) {
-  return <input ref={ref} type={type} className={className} {...props} />;
-});
